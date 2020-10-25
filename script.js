7fcbaad1a79bc6ae046f15da6905b8b8a9d3e7ae
Casey <casey.gardonio@foat.org>
2020-10-25 16:17:30 -0500

    first commit; completed variable creation and entry validation for numbers

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-// Write your JavaScript code here!
+window.addEventListener('load', function() {
+   let launchForm = document.getElementById('launchForm');
+
+   let pilotName = String(document.querySelector('input[name=pilotName]')); 
+   let copilotName = String(document.querySelector('input[name=copilotName]')); 
+   let fuelLevel = document.querySelector('input[name=fuelLevel]'); 
+   let cargoMass = document.querySelector('input[name=cargoMass]'); 
+
+   launchForm.addEventListener("submit", function(event) {
+      if(pilotName.value ==='' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value===''){
+         window.alert('Please enter a value for all fields!');
+         event.preventDefault;
+      };
+      // if (typeof pilotName.value == 'string' || typeof copilotName.value == 'string') {
+      //    window.alert('Please enter a valid name for the Pilot and/or Co-Pilot.');
+      //    event.preventDefault;
+      // };
+      if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+         window.alert('Please enter a valid number for Fuel Level and/or Cargo Mass.');
+         event.preventDefault;
+      }
+
+      document.getElementById('pilotStatus').innerHTML = `Pilot Ready: ${pilotName.value}`;
+      document.getElementById('copilotStatus').innerHTML = `Co-Pilot Ready: ${copilotName.value}`;
+
+   });
+
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
