cac87144be01d408e9c8b3146596167977ad55a8
Casey <casey.gardonio@foat.org>
2020-10-28 20:26:17 -0500

    completed bonus mission and launch readiness box; need to work on conditionals

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 window.addEventListener('load', function() {
    console.log('window loaded');
-   //Question: does everything else need to go *inside* the window.load function? 
-   //or can I close this function and put everything else after it in its own function?
 
    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
       response.json().then(function(json) {
-         console.log(json);
-            
+
+         let randomTarget = Math.floor(Math.random()*json.length);
          const missionTarget = document.getElementById("missionTarget");
+         
          missionTarget.innerHTML = `
             <h2>Mission Destination</h2>
                <ol>
-                  <li>Name: ${json[2].name}</li>
-                  <li>Diameter: ${json[2].diameter}</li>
-                  <li>Star: ${json[2].star}</li>
-                  <li>Distance from Earth: ${json[2].distance}</li>
-                  <li>Number of Moons: ${json[2].moons}</li>
+                  <li>Name: ${json[randomTarget].name}</li>
+                  <li>Diameter: ${json[randomTarget].diameter}</li>
+                  <li>Star: ${json[randomTarget].star}</li>
+                  <li>Distance from Earth: ${json[randomTarget].distance}</li>
+                  <li>Number of Moons: ${json[randomTarget].moons}</li>
                </ol>
-            <img src="${json[2].image}"></img>
+            <img src="${json[randomTarget].image}"></img>
          `;
-      });
+      });  
    });
-
+   
+   let form = document.querySelector('form')
    let launchForm = document.getElementById('launchForm');
    let button = document.getElementById('formSubmit');
    let pilotName = document.querySelector('input[name=pilotName]'); 
@@ -29,54 +29,44 @@ window.addEventListener('load', function() {
    let fuelLevel = document.querySelector('input[name=fuelLevel]'); 
    let cargoMass = document.querySelector('input[name=cargoMass]'); 
 
-   button.addEventListener("click", function(event) {
-
-      console.log(typeof pilotName.value, typeof copilotName.value, typeof fuelLevel.value, typeof cargoMass.value);
+   form.addEventListener("submit", function(event) { 
+      event.preventDefault();
+      //n.b. preventDefault needs to go at the beginning of the function instead of inside each conditional
 
       if(pilotName.value ==='' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value===''){
          window.alert('Please enter a value for all fields!');
-         event.preventDefault;
       };  
-
-      // // need help getting this validation to work: 
-      // if (typeof pilotName.value === 'number' || typeof copilotName.value === 'number') {
+    
       if (!isNaN(pilotName.value) || !isNaN(copilotName.value)) { 
          window.alert('Please enter a valid name for the Pilot and/or Co-Pilot.');
-         event.preventDefault;
       };
-      // // console.log statement shows that all fields return a string, even if a number is entered, so this alert is never triggered
       
       if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
          window.alert('Please enter a valid number for Fuel Level and/or Cargo Mass.');
-         event.preventDefault;
       };
-   });
 
-   launchForm.addEventListener('submit', function(event) {
-
-      console.log(pilotName.value + copilotName.value);
-      let pilotStatus = document.getAnimations('pilotStatus');
-      pilotStatus.innerHTML = `Pilot ${pilotName.value} is Ready`;
-         // document.getElementById('pilotStatus').innerHTML = `Pilot Ready: ${pilotName.value}`;
-         // document.getElementById('copilotStatus').innerHTML = `Co-Pilot Ready: ${copilotName.value}`;
-
-      if (Number(fuelLevel.value) <= 10000) {
-         document.getElementById('faultyItems').style.visibility = 'visible';
+      document.getElementById('faultyItems').style.visibility = 'visible';
+      document.getElementById('pilotStatus').innerHTML = `Pilot ${pilotName.value} is ready.`;
+      document.getElementById('copilotStatus').innerHTML = `Co-Pilot ${copilotName.value} is ready.`;
+      document.getElementById('launchStatus').innerHTML = `Shuttle not ready for launch.`
+      document.getElementById('launchStatus').style.color = 'red';
+      document.getElementById('cargoMass').innerHTML = `Too much cargo. Please enter a value less than 10,000 kg.`;
+      
+      if (fuelLevel.value < 10000) {
+         console.log('too little fuel')
          document.getElementById('fuelStatus').innerHTML = `Not enough fuel. Please enter a value of 10,000 liters or more.`;
-         document.getElementById('launchStatus').innerHTML = `Shuttle not ready for launch.`
-         document.getElementById('launchStatus').style.color = 'red';
-      } else if (Number(cargoMass.value)>=10000){
-         document.getElementById('faultyItems').style.visibility = 'visible';
-         document.getElementById('fuelStatus').innerHTML = `Too much cargo. Please enter a value less than 10,000 kg.`;
-         document.getElementById('launchStatus').innerHTML = `Shuttle not ready for launch.`
-         document.getElementById('launchStatus').style.color = 'red';
+      } else if (Number(cargoMass.value)> 10000){
+         console.log('too much cargo');
+         // document.getElementById('cargoMass').innerHTML = `Too much cargo. Please enter a value less than 10,000 kg.`;
       } else {
-         document.getElementById('faultyItems').style.visibility = 'visible';
+         console.log('enough fuel and little enough cargo')
          document.getElementById('launchStatus').innerHTML = `Shuttle is ready for launch.`
          document.getElementById('launchStatus').style.color = 'green';
-      };
-      //come back and refactor this
+      }
       
-      });
+      
+      
+      
+   });
 
 });
